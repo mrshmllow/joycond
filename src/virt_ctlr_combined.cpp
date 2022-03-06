685b7b946f064a89079351065d3d9e6ed4c69a2c
marshmallow <marshycity@gmail.com>
2022-03-07 09:59:14 +1100

    Swap buttons

--- src/virt_ctlr_combined.cpp
+++ src/virt_ctlr_combined.cpp
@@ -51,6 +51,16 @@ void virt_ctlr_combined::relay_events(std::shared_ptr<phys_ctlr> phys)
                 libevdev_uinput_write_event(uidev, EV_ABS, ABS_RZ, ev.value);
                 ret = libevdev_next_event(evdev, LIBEVDEV_READ_FLAG_NORMAL, &ev);
                 continue;
+            } else if (phys == physr && ev.type == EV_KEY && (ev.code == BTN_A || ev.code == BTN_B)) {
+                /* Swap A, B buttons */
+                libevdev_uinput_write_event(uidev, ev.type, ev.code == BTN_A ? BTN_B: BTN_A, ev.value);
+                ret = libevdev_next_event(evdev, LIBEVDEV_READ_FLAG_NORMAL, &ev);
+                continue;
+            } else if (phys == physr && ev.type == EV_KEY && (ev.code == BTN_Y || ev.code == BTN_X)) {
+                /* Swap X, Y buttons */
+                libevdev_uinput_write_event(uidev, ev.type, ev.code == BTN_Y ? BTN_X: BTN_Y, ev.value);
+                ret = libevdev_next_event(evdev, LIBEVDEV_READ_FLAG_NORMAL, &ev);
+                continue;
             }
 
             if (ev.type == EV_KEY) {
